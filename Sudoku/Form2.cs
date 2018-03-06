022bb7c3ac18df7c2f52155af88c667d236e482f
Adrian <Adrian@ADRIAN-MAIN>
2018-03-06 08:22:45 +0100

    emergency commit

--- Sudoku/Form2.cs
+++ Sudoku/Form2.cs
@@ -34,7 +34,6 @@ namespace Sudoku
             TableLayoutPanel obecnyKwadrat;
             for (int i = 0; i < 9; ++i)
             {
-                //Controls.Find()
                 obecnyKwadratCtrl = tableLayoutPanelPlansza.Controls.Find("tableLayoutPanel" + i, false)[0];
                 obecnyKwadratCtrl.TabIndex = i;
                 obecnyKwadrat = obecnyKwadratCtrl as TableLayoutPanel;
@@ -49,16 +48,82 @@ namespace Sudoku
                 }
             }
         }
-
+        /*
         private void GenerujSudoku()
         {
-            int wiersz = 0;
+            int nowa;
+            int[,] plansza = new int[9, 9];
+            List<int>[,] juzUzyte = new List<int>[9, 9];
+            for (int i = 0; i < 9; ++i)
+                for (int j = 0; j < 9; ++j)
+                    juzUzyte[i, j] = new List<int>();
+            List<int>[] dostepneWKwadracie = new List<int>[9];
+            for (int i = 0; i < 9; ++i)
+                dostepneWKwadracie[i] = new List<int>(Enumerable.Range(1, 9));
+            Random generatorLiczb = new Random();
+            List<int> dostepneWartosci = new List<int>(Enumerable.Range(1, 9));
+            for (int i = 0; i < 9; ++i)
+            {
+                nowa = dostepneWartosci[generatorLiczb.Next(0, dostepneWartosci.Count)];
+                dostepneWartosci.Remove(nowa);
+                plansza[0, i] = nowa;
+                dostepneWKwadracie[i / 3].Remove(nowa);
+                juzUzyte[0, i].Add(nowa);
+            }
+            int v = 1, h = 0;
+            while (v < 9)
+            {
+                dostepneWartosci = new List<int>(Enumerable.Range(1, 9));
+                for (int j = 0; j < 9; ++j)
+                {
+                    dostepneWartosci.Remove(plansza[j, h]);
+                    dostepneWartosci.Remove(plansza[v, j]);
+                }
+                foreach(int number in Enumerable.Range(1, 9).Except(dostepneWKwadracie[v / 3 * 3 + h / 3]))
+                {
+                    dostepneWartosci.Remove(number);
+                }
+                foreach(int number in juzUzyte[v, h])
+                {
+                    dostepneWartosci.Remove(number);
+                }
+
+                if (dostepneWartosci.Count > 0)
+                {
+                    nowa = dostepneWartosci[generatorLiczb.Next(0, dostepneWartosci.Count)];
+                    plansza[v, h] = nowa;
+                    dostepneWKwadracie[v / 3 * 3 + h / 3].Remove(nowa);
+                    juzUzyte[v, h].Add(nowa);
+                }
+                else
+                {
+                    juzUzyte[v, h].Clear();
+                    if (plansza[v, h] != 0)
+                        dostepneWKwadracie[v / 3 * 3 + h / 3].Add(plansza[v, h]);
+                    plansza[v, h] = 0;
+                    --h;
+                    if (h < 0)
+                    {
+                        --v;
+                        h += 9;
+                    }
+                    continue;
+                }
+
+                ++h;
+                if (h >= 9)
+                {
+                    h -= 9;
+                    v++;
+                }
+            }
+            /*int wiersz = 0;
             int kolumna = 0;
             int nowaWartosc;
             List<int> wartosci = new List<int>(Enumerable.Range(1, 9));
             List<int> uzyteWartosci = new List<int>();
             Random generator = new Random();
-            int losowyIndeks = generator.Next(0, 9);
+            int losowyIndeks = generator.Next(0, wartosci.Count);
             nowaWartosc = wartosci[losowyIndeks];
             tabelkaSudoku[wiersz, kolumna].InicjujPole(nowaWartosc);
             wartosci.Remove(nowaWartosc);
@@ -71,8 +136,14 @@ namespace Sudoku
                 tabelkaSudoku[wiersz, kolumna].InicjujPole(nowaWartosc);
             }
             wiersz = 0;
-        }
+            
+        }*/
 
+        private void GenerujSudoku()
+        {
+
+        }
+        
         private void buttonZapiszStan_Click(object sender, EventArgs e)
         {
             MenedzerZapisuOdczytu.Zapisz(tabelkaSudoku, saveSudokuDialog);

fc5918ce770de2ad5df5217d4277a1dd8ffeecc0
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2021-09-24 13:09:46 +0200

    using directives cleanup

--- Sudoku/Interfejsy/IKomunikaty.cs
+++ Sudoku/Interfejsy/IKomunikaty.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sudoku.Interfejsy
+﻿namespace Sudoku.Interfejsy
 {
     interface IKomunikaty
     {
-        #region //Właściwości
         string Uwaga { get; }
         string NieodpowiedniaIloscLinii { get; }
-        #endregion
 
-
-        #region //Metody
         string NieodpowiedniaIloscWartosciPol(int nrLinii);
         string NieWszystkieSaCyframi(int nrLinii);
-        #endregion
     }
 }
--- Sudoku/Klasy/Jezyk.cs
+++ Sudoku/Klasy/Jezyk.cs
@@ -1,9 +1,4 @@
 ﻿using Sudoku.Interfejsy;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sudoku
 {

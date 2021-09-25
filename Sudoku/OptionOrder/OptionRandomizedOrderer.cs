7ad70a4fb8fb5b116bd26b8d29142b7a35428026
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2021-09-25 13:35:33 +0200

    Removed SwapHelper replaced by (a,b) = (b,a);

--- Sudoku/OptionOrder/OptionRandomizedOrderer.cs
+++ Sudoku/OptionOrder/OptionRandomizedOrderer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using SudokuLib.Helpers;
 
 namespace SudokuLib.OptionOrder
 {
@@ -14,7 +13,10 @@ namespace SudokuLib.OptionOrder
             var orderedArray = enumerable.ToArray();
             int count = orderedArray.Count();
             for (int i = count - 1; i > 1; i--)
-                SwapHelper.Swap(ref orderedArray[i], ref orderedArray[_random.Next(i)]);
+            {
+                int randomIndex =_random.Next(i);
+                (orderedArray[i], orderedArray[randomIndex]) = (orderedArray[randomIndex], orderedArray[i]);
+            }
             return orderedArray;
         }
     }
